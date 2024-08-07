2822cd3136dbc78ab98175de4cd97f1037adc2bd
unknown <phucphan25051999@gmail.com>
2024-08-07 11:13:27 +0700

    [P] adding get rooms, create rooms, get messages, send message API

--- server.js
+++ server.js
@@ -14,10 +14,39 @@ const io = new Server(server, {
 })
 
 app.use(cors());
+app.use(express.json());
+
+let rooms = {};
+
+
+app.get('/rooms', (req, res) => {
+    res.json(Object.keys(rooms))
+});
+
+app.post('/rooms', (req, res) => {
+    const { room } = req.body;
+
+    if(rooms[room]) {
+        return res.status(400).json({message: 'Error: room is aldready exist'});
+    }
+
+    rooms[room] = [];
+    res.status(201).json({ message: 'Successfully: create a room'});
+})
 
 io.on('connection', (socket) => {
-    socket.on('message', (m) => {
-        io.emit('message', m);
+
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+        socket.emit('existingRoomMessages', rooms[room] || []);
+    })
+
+    socket.on('message', ({room, message}) => {
+        if(!rooms[room]) rooms[room] = [];
+
+        let newMess = {id: socket.id, message}
+        rooms[room].push(newMess);
+        io.to(room).emit('message', newMess);
     });
 
     socket.on('disconnect', () => {
